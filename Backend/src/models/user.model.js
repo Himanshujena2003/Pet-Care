33f9ebe0aef95a4f078b58434d39d9cc14a760ed
Himanshujena2003 <himanshujena2003@gmail.com>
2025-01-10 15:23:03 +0530

    updated model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -32,8 +32,4 @@ function userModel(){
     user = mongoose.model("User",userSchema)
 }
 
-// userSchema.methods.isValidPassword = async function(password){
-//     return await bcrypt.compare(password,this.password); 
-// };
-
 export {userModel,user}
